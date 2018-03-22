4c44d915c49e77ae42c18a70ab0f8b3e56360703
Noah <Esrelith@users.noreply.github.com>
2018-03-22 12:32:14 -0500

    adds text to level select and more (future) levels to chose from

--- Assets/scripts/PlayerMovement.cs
+++ Assets/scripts/PlayerMovement.cs
@@ -6,15 +6,10 @@ public class PlayerMovement : MonoBehaviour {
 
 	Rigidbody player;
 	private float moveSpeed = 42.5f;
-	public float jumpSpeed = 10000f;
-
 	public float voidHeight;
-	
-	public Vector3 boostMoveSpeed = new Vector3 (20,0,20);
 	Vector3 movement = Vector3.zero;
 
-
-	public bool isGrounded { get; set; }
+	
 
 	// Use this for initialization
 	void Start ()
